1866a039c4ad9adec0651fa7a953ebbbe457bb9f
Dorwink <lanore.corentin@gmail.com>
2022-03-10 16:43:31 +0100

    Ultimate omzefoujzegmbuje

--- classe.py
+++ classe.py
@@ -26,13 +26,52 @@ class HMM:  # Hidden markov chain with unidimensional symbol with discrete distr
         self.A = 1 / N * np.ones((N, N))
         self.B = 1 / M * np.ones((N, M))
 
+    def backward_bis(self,Y):
+        
+        N = self.N  # Oh c'est tellement plus simple que de remettre self.N a chaque fois ...
+        M = self.M
+        
+        T = Y.shape[0]
+        beta = np.zeros((self.N, Y.shape[0]))
+        
+        beta[:,T-1] = 1
+        
+        for t in range(T-2,-1,-1):
+            for i in range(N):
+                inter = 0
+                for j in range(N):
+                    inter+=beta[j,t+1]*self.A[i,j]*self.B[j,Y[t+1]]
+                beta[i,t]=inter
+            
+        return beta            
+
+    def forward_bis(self,Y):
+        
+        N = self.N  # Oh c'est tellement plus simple que de remettre self.N a chaque fois ...
+        M = self.M
+        
+        T= Y.shape[0]
+        alpha = np.zeros((self.N, Y.shape[0]))
+        
+        alpha[:,0] = self.pi[:] * self.B[:,Y[0]]
+        
+        
+        for t in range(0,T-1):
+            for i in range(N):
+                inter = 0
+                for j in range(N):
+                    inter+=alpha[j,t]*self.A[j,i]
+                alpha[i,t+1]=self.B[i,Y[t+1]]*inter 
+                
+        return alpha
+
     def forward(self, Y):
         """
         :param Y: sequence (y1, ..., yT)
         :return: alpha, shape (M, T), alpha_i_t being the proba of seeing (y1, ..., yt)
          and being at state i at time t
         """
-        alpha = np.zeros((self.M, Y.shape[0]))
+        alpha = np.zeros((self.N, Y.shape[0]))
         alpha[:, 0] = np.einsum('i,i->i', self.pi, self.B[:, Y[0]])
         for t in range(1, Y.shape[0]):
             alpha[:, t] = np.einsum('i,i->i', self.B[:, Y[t]], np.einsum('j,ji->i', alpha[:, t-1], self.A))
@@ -45,7 +84,7 @@ class HMM:  # Hidden markov chain with unidimensional symbol with discrete distr
          knowing that we are in state i at time t
         """
         T = Y.shape[0]
-        beta = np.zeros((self.M, Y.shape[0]))
+        beta = np.zeros((self.N, Y.shape[0]))
         beta[:, T-1] = 1
         for t in range(T-2, -1, -1):
             beta[:, t] = np.einsum('j,ij,j->i', beta[:, t+1], self.A, self.B[:, Y[t+1]])
@@ -58,6 +97,7 @@ class HMM:  # Hidden markov chain with unidimensional symbol with discrete distr
 
         T = Y.shape[0]  # Data length
 
+        
         alpha, beta = self.forward(Y), self.backward(Y)
 
         ##UPDATE
@@ -65,7 +105,7 @@ class HMM:  # Hidden markov chain with unidimensional symbol with discrete distr
         # compute gamma
         gamma = np.zeros((N, T))
 
-        for t in range(T - 1):
+        for t in range(T):
             inter = 0
 
             # compute the intermediate component
@@ -78,7 +118,7 @@ class HMM:  # Hidden markov chain with unidimensional symbol with discrete distr
 
         # Compute xsi
 
-        xsi = np.zeros((N, N, T))
+        xsi = np.zeros((N, N, T-1))
 
         for t in range(T - 1):
 
@@ -99,7 +139,7 @@ class HMM:  # Hidden markov chain with unidimensional symbol with discrete distr
 
         for i in range(N): #Update transition matrix
             for j in range(N):
-                self.A[i, j] = np.sum(xsi[:,:,:-1], axis=-1)[i, j] / np.sum(gamma[i,:-1], -1)
+                self.A[i, j] = np.sum(xsi, axis=-1)[i, j] / np.sum(gamma[i,:-1], -1)
                 
         
         for i in range(N): #Update symbol generation
@@ -112,21 +152,96 @@ class HMM:  # Hidden markov chain with unidimensional symbol with discrete distr
                     inter2+=gamma[i,t]
                 
                 self.B[i,j]= inter1 / inter2
+                
+    def Baum_welch_sequence(self, Y):  # Given a sample Y = (y1,....yL), what are the new parameters ?
+
+        N = self.N  # Oh c'est tellement plus simple que de remettre self.N a chaque fois ...
+        M = self.M
+
+        T = Y.shape[0]
+        R = Y.shape[1]# number of sequences length
+
+        
+
+        
+        
+
+        ##UPDATE
+
+        # compute gamma
+        gamma = np.zeros((N, T,R))
+        xsi = np.zeros((N, N, T-1,R))
+        
+        for r in range(R):
+        
+            alpha, beta = self.forward(Y[:,r]), self.backward(Y[:,r])
+            
+            for t in range(T):
+                inter = 0
+    
+                # compute the intermediate component
+                for j in range(N):
+                    inter += alpha[j, t] * beta[j, t]
+    
+                for i in range(N):
+                    gamma[i, t,r] = alpha[i, t] * beta[i, t] / inter
+                    
+    
+            # Compute xsi
+    
+            for t in range(T - 1):
+    
+                inter = 0
+    
+                for k in range(N):
+                    for w in range(N):
+                        inter += alpha[k, t] * self.A[k, w] * beta[w, t + 1] * self.B[w, Y[t + 1,r]]
+    
+                for i in range(N):
+                    for j in range(N):
+    
+                        xsi[i, j, t,r] = alpha[i, t] * self.A[i, j] * beta[j, t + 1] * self.B[j, Y[t + 1,r]] / inter
+
+                    # UPDATE THE PARAMETER
+
+        self.pi[:] = np.sum(gamma[:, 0,:],axis=-1) / R #update pi
+
+        for i in range(N): #Update transition matrix
+            for j in range(N):
+                self.A[i, j] = np.sum(xsi, axis=(-1,-2))[i, j] / np.sum(gamma[i,:-1], axis=(-1,-2))
+                
+        
+        for i in range(N): #Update symbol generation
+            for j in range(M):
+                inter1 = 0
+                inter2 = 0
+                for r in range(R):
+                    for t in range(T-1):
+                        if(Y[t,r]==j):
+                            inter1+=gamma[i,t,r]
+                        inter2+=gamma[i,t,r]
+                
+                self.B[i,j]= inter1 / inter2           
 
 
 hmm = HMM(2, 2)
-hmm.pi = np.array([0.2, 0.8])
+hmm.pi = np.array([0.5, 0.5])
 
-hmm.A = np.array([[0.2, 0.9], [0.8, 0.1]])
-hmm.init_parameter()
-print(hmm.A)
-hmm.B = np.array([[0.2, 0.9], [0.8, 0.1]])
-print
+hmm.A = np.array([[0.6, 0.4], [0.3, 0.7]])
+hmm.B = np.array([[0.4, 0.6], [0.25, 0.75]])
 
 
-X, Y = CoinToss(20)
-print(X)
-hmm.Baum_welch(Y)
+T_0 = 100 #number of coin toss
+R_0 = 10000 #number of trials
+
+Y_seq = np.zeros((T_0,R_0),dtype=int)
+
+for r in range(R_0):
+    X, Y = CoinToss(T_0)
+    Y_seq[:,r]=Y
+
+
+hmm.Baum_welch_sequence(Y_seq)
 
 print("pi", hmm.pi)
 print("A", hmm.A)
--- generate_coin_toss.py
+++ generate_coin_toss.py
@@ -2,28 +2,31 @@ import numpy as np
 
 def CoinToss(T):
     Y = np.zeros(T, dtype=int)
-    p = 0.2  # ☻proba of biased coin
+    p = 0.9  # ☻proba of biased coin
     pi = np.array([0.8, 0.2])  # proba of initial state
-    A = np.array([[0.5, 0.5], [0.5, 0.5]])
-    B = np.array([[p, 0.5], [1 - p, 0.5]])
+    A = np.array([[0.8, 0.2], [0.1, 0.9]])
+    B = np.array([[p, 1-p], [0.5, 0.5]])
 
-    X = np.zeros(T)
+
+    X = np.zeros(T,dtype=int)
 
     if (np.random.rand() < pi[0]):  # Init first coin toss
         X[0] = 0
     else:
         X[0] = 1
 
-    if (np.random.rand() < B[0, int(X[0])]):
+    if (np.random.rand() < B[X[0], 0]):
         Y[0] = 0
     else:
         Y[0] = 1
 
     for t in range(1, T):
         # Transition
-        if (np.random.rand() < A[int(X[t - 1]), 1 - int(X[t - 1])]):
+        if (np.random.rand() < A[X[t - 1], 1 - X[t - 1]]):
             X[t] = 1 - X[t - 1]
-        if (np.random.rand() < B[0, int(X[t])]):
+            
+        #Symbol emission
+        if (np.random.rand() < B[X[t],0]):
             Y[t] = 0
         else:
             Y[t] = 1
