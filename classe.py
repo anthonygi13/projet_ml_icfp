f479405bc400ce353b7b9dfbf8e67d448bd1a577
Anthony Giraudo <giraudo@chalmers.se>
2022-03-11 15:49:35 +0100

    code rapide

--- classe.py
+++ classe.py
@@ -122,6 +122,31 @@ class HMM:  # Hidden markov chain with unidimensional symbol with discrete distr
             beta[:, t] = np.einsum('j,ij,j->i', beta[:, t+1], self.A, self.B[:, Y[t+1]])
         return beta
 
+    def forward_seq(self, Y_seq):
+        """
+        :param Y_seq: sequences, shape (R, T)
+        :return: alpha, shape (R, N, T), alpha_rit being the proba of seeing (y_r1, ..., y_rt)
+         and being at state i at time t
+        """
+        alpha = np.zeros((Y_seq.shape[0], self.N, Y_seq.shape[1]))
+        alpha[:, :, 0] = np.einsum('i,ir->ri', self.pi, self.B[:, Y_seq[:, 0]])
+        for t in range(1, Y_seq.shape[1]):
+            alpha[:, :, t] = np.einsum('ir,ri->ri', self.B[:, Y_seq[:, t]], np.einsum('rj,ji->ri', alpha[:, :, t-1], self.A))
+        return alpha
+
+    def backward_seq(self, Y_seq):
+        """
+        :param Y_seq: sequences, shape (R, T)
+        :return: beta, shape (R, N, T), beta_rit being the proba of seeing (y_r(t+1), ..., y_rT)
+         knowing that we are in state i at time t
+        """
+        T = Y_seq.shape[1]
+        beta = np.zeros((Y_seq.shape[0], self.N, T))
+        beta[:, :, T-1] = 1
+        for t in range(T-2, -1, -1):
+            beta[:, :, t] = np.einsum('rj,ij,jr->ri', beta[:, :, t+1], self.A, self.B[:, Y_seq[:, t+1]])
+        return beta
+
     def probability(self, Y):
         """
         :param Y: sequence (y1, ..., yT)
@@ -130,26 +155,28 @@ class HMM:  # Hidden markov chain with unidimensional symbol with discrete distr
         alpha = self.forward(Y)
         return np.sum(alpha[:, -1])
 
+    def logv(self, Y_seq):
+        """
+        :param Y_seq: sequences, shape (R, T)
+        :return: logvraisemblance de Y_seq
+        """
+        alpha = self.forward_seq(Y_seq)
+        probas = np.sum(alpha[:, :, -1], axis=1)
+        return np.sum(np.log(probas))
+
     def BW_bis(self, Y_seq):
         R = Y_seq.shape[0]
-        T = Y_seq.shape[1]
-        gamma_tot = np.zeros((R, self.N, T))
-        xsi_tot = np.zeros((R, self.N, self.N, T-1))
-        for r, Y in enumerate(Y_seq):  # TODO: virer cette boucle
-            alpha, beta = self.forward(Y), self.backward(Y)
-            gamma = alpha * beta
-            gamma /= np.sum(gamma, axis=0)
-            xsi = np.einsum("it,ij,jt,jt->ijt", alpha[:, :-1], self.A, beta[:, 1:], self.B[:, Y[1:]])
-            xsi /= np.sum(xsi, axis=(0, 1))
-            gamma_tot[r] = gamma
-            xsi_tot[r] = xsi
-        self.pi = np.sum(gamma_tot[:, :, 0], axis=0) / R
-        self.A = (np.sum(xsi_tot, axis=(0, -1)).T / np.sum(gamma_tot[:, :, :-1], axis=(0, -1))).T
-        #print(np.tile(np.arange(self.M), (Y_seq.shape[0], Y_seq.shape[1], 1)).shape)
-        #print(np.einsum("ijk->jki", np.tile(Y_seq, (self.M, 1, 1))).shape)
+        alpha, beta = self.forward_seq(Y_seq), self.backward_seq(Y_seq)
+
+        gamma = alpha * beta
+        gamma = np.einsum("rit,rt->rit", gamma, 1/np.sum(gamma, axis=1))
+        xsi = np.einsum("rit,ij,rjt,jrt->rijt", alpha[:, :, :-1], self.A, beta[:, :, 1:], self.B[:, Y_seq[:, 1:]])
+        xsi = np.einsum("rijt,rt->rijt", xsi, 1/np.sum(xsi, axis=(1, 2)))
+
+        self.pi = np.sum(gamma[:, :, 0], axis=0) / R
+        self.A = (np.sum(xsi, axis=(0, -1)).T / np.sum(gamma[:, :, :-1], axis=(0, -1))).T
         mask = (np.tile(np.arange(self.M), (Y_seq.shape[0], Y_seq.shape[1], 1)) == np.einsum("ijk->jki", np.tile(Y_seq, (self.M, 1, 1))))
-        #print(mask.shape)
-        self.B = (np.einsum("rtj,rit->ij", mask, gamma_tot).T / np.sum(gamma_tot, axis=(0, -1))).T
+        self.B = (np.einsum("rtj,rit->ij", mask, gamma).T / np.sum(gamma, axis=(0, -1))).T
 
 
     def Baum_welch_sequence(self, Y):  # Given a sample Y = (y1,....yL), what are the new parameters ?
@@ -220,7 +247,6 @@ class HMM:  # Hidden markov chain with unidimensional symbol with discrete distr
 
 hmm = HMM(2, 2)
 hmm.pi = np.array([0.4, 0.6])
-
 hmm.A = np.array([[0.3, 0.7], [0.7, 0.3]])
 hmm.B = np.array([[0.7, 0.3], [0.3, 0.7]])
 
@@ -232,25 +258,17 @@ print("____________")
 T_0 = 100 #number of coin toss
 R_0 = 300 #number of trials
 
-
 Y_seq = np.zeros((R_0, T_0), dtype=int)
 
 for r in range(R_0):
     X, Y = CoinToss(T_0)
     Y_seq[r, :] = Y
 
-logvs = []
-logv = 0
-for Y in Y_seq:
-    logv += np.log(hmm.probability(Y))
-logvs += [logv]
+logvs = [hmm.logv(Y_seq)]
 
-for i in range(20):
+for i in range(200):
     hmm.BW_bis(Y_seq)
-    logv = 0
-    for Y in Y_seq:
-        logv += np.log(hmm.probability(Y))
-    logvs += [logv]
+    logvs += [hmm.logv(Y_seq)]
 
 plt.scatter(np.arange(len(logvs)), logvs)
 plt.show()
@@ -258,3 +276,18 @@ plt.show()
 print("pi", hmm.pi)
 print("A", hmm.A)
 print("B", hmm.B)
+
+"""
+print("bw corentin")
+
+hmm = HMM(2, 2)
+hmm.pi = np.array([0.4, 0.6])
+hmm.A = np.array([[0.3, 0.7], [0.7, 0.3]])
+hmm.B = np.array([[0.7, 0.3], [0.3, 0.7]])
+
+hmm.Baum_welch_sequence(Y_seq.T)
+
+print("pi", hmm.pi)
+print("A", hmm.A)
+print("B", hmm.B)
+"""
